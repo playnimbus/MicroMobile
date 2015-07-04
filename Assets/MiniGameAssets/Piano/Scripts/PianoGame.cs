f35e542b5069054f480c7e4dfb84e6edf534f3eb
Nick Mudry <nick@mudry.me>
2015-07-04 14:18:10 -0400

    PianoGame Work In Progress
    Work in progress of Piano Game

---
+++ Assets/MiniGameAssets/Piano/Scripts/PianoGame.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PianoGame : MonoBehaviour {
+
+    public GameObject[] row1, row2, row3, row4, row5, row6, row7, row8;
+    public GameObject[] rows;
+    float timer;
+    public float gameTime;
+	// Use this for initialization
+	void Start () {
+        RandomTile(row1);
+        RandomTile(row2);
+        RandomTile(row3);
+        RandomTile(row4);
+        RandomTile(row5);
+        RandomTile(row6);
+        RandomTile(row7);
+        RandomTile(row8);
+        ScoreKeeper.GameStarted = true;
+        rows[0].BroadcastMessage("IsCurrentRow", SendMessageOptions.DontRequireReceiver);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      //  if (ScoreKeeper.GameStarted)
+        {
+            GameStartedUpdate();
+        }
+	}
+
+    void RandomTile(GameObject[] row) 
+    {
+        int randomTile = Random.Range(0, 3);
+        row[randomTile].gameObject.GetComponent<PianoTile>().isBlackTile = true;
+    }
+
+    void GameStartedUpdate()
+    {
+        timer += Time.deltaTime;
+    }
+}
---
+++ Assets/MiniGameAssets/Piano/Scripts/PianoTile.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PianoTile : MonoBehaviour {
+    public bool isBlackTile;
+    public bool isCurrentRow;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        
+	}
+    void IsCurrentRow()
+    {
+        isCurrentRow = true;
+    }
+	
+	// Update is called once per frame
+    void Update()
+    {
+        if (isBlackTile)
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = Color.black;
+        }
+
+        if (ScoreKeeper.GameStarted)
+        {
+            GameStartedUpdate();
+        }
+    }
+
+    void GameStartedUpdate()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        if (ScoreKeeper.GameStarted && isBlackTile && isCurrentRow)
+        {
+            Debug.Log("Win");
+        }
+        if (ScoreKeeper.GameStarted && !isBlackTile && isCurrentRow)
+        {
+            Debug.Log("fail");
+        }
+    }
+    
+
+
+}
