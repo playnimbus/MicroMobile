a5e5c1adb833c6d9775082f7e80ab5d462ab4dde
Nick Mudry <nick@mudry.me>
2015-07-04 00:50:01 -0400

    Flappy MiniGame + Centering Fixes
    :bird: :flags: :bug:

---
+++ Assets/MiniGameAssets/Flap/Scripts/MoveObjects.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveObjects : MonoBehaviour {
+    public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void Update()
+    {
+        if (ScoreKeeper.GameStarted)
+        {
+            UpdateStartedGame();
+        }
+    }
+
+    void UpdateStartedGame()
+    {
+            gameObject.transform.Translate(Time.deltaTime * speed * -1f, 0f, 0f);
+    }
+}
---
+++ Assets/MiniGameAssets/Flap/Scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ScoreKeeper.GameStarted)
+        {
+            UpdateStartedGame();
+        }
+    }
+
+    void UpdateStartedGame()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouse Down");
+            gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 250f);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Fail")
+        {
+            ScoreKeeper.GameFailed();
+        }
+        if (col.gameObject.tag == "Win")
+        {
+            ScoreKeeper.GameWon();
+        }
+    }
+}
